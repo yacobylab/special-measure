630fd406aae01fce98688cac7872ff5aabea81b3
spharvey3 <spharvey3@gmail.com>
2019-01-09 18:34:50 -0500

    Fix smnext to work in case of no files in folder.

--- sm/scan/smnext.m
+++ sm/scan/smnext.m
@@ -9,9 +9,7 @@ function [nextstr,nextnum]=smnext(name, opts)
 % If not using with smrun, necessary to preface file with sm_, as in 
 % save(['sm_' smnext('filename')],'data')
 
-global smn_lastname; 
-global smn_lastfile;
-global smn_lastnum;
+global smn_lastname; global smn_lastfile; global smn_lastnum;
 
 if exist('name','var')
     name=[name '_'];
@@ -19,9 +17,7 @@ else
     name='';
 end
 
-if ~exist('opts','var') || isempty(opts)
-    opts = '';
-end
+if ~exist('opts','var') || isempty(opts), opts = ''; end
 
 % If smn_lastnum exists, just check that the file exists, and that the file
 % with the num smn_lastnum+1 does not. 
@@ -36,32 +32,35 @@ if exist('smn_lastnum','var') && ~isempty('smn_lastnum')
     files1=dir(sprintf('sm*%04d.mat',nextnum));
     files2=dir(sprintf('sm*%04d.mat',nextnum-1));
     if isempty(files1) && ~isempty(files2)
-      search=0;
+      search=0;      
     end
 end
 
 % if smn_lastnum doesn't exist or the requisite files suggest it's not
 % correct, look at all the files in directory and find largest number, then
-% increment to get next number. 
+% increment to get next number.
 if search
     files = dir('sm*.mat');
-    numsCell=regexp([files.name],'(\d{4}).mat','tokens');
-    nums = cellfun(@str2num,[numsCell{:}]);
-    lastnum = max(nums); 
-    nextnum = lastnum+1;     
+    if isempty(files)
+        nextnum = 1;
+    else
+        numsCell=regexp([files.name],'(\d{4}).mat','tokens');
+        nums = cellfun(@str2num,[numsCell{:}]);
+        lastnum = max(nums);
+        nextnum = lastnum+1;
+    end
 end
 
 nextstr=sprintf('%s%04d',name,nextnum);
 
-if isempty(strfind(opts, 'quiet'))    
+if ~isopt(opts,'quiet')
     fprintf('Next file: %s\n',nextstr);
 end
 
-if isempty(strfind(opts, 'nocutbuffer'))
+if ~isopt(opts, 'nocutbuffer')
     clipboard('copy',nextstr);
 end
 smn_lastname=name;
 smn_lastfile=sprintf('sm_%s.mat',nextstr);
 smn_lastnum=nextnum;
-
 end
