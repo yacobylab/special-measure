4344c7db36e76bc89c3d70fef289e80a032a97da
spharvey3 <spharvey3@gmail.com>
2019-03-21 17:57:20 -0400

    Change path names for labBrick to be more general, for easier use on multiple computers.

--- sm/insts/labbrick/labBrickLoadLibrary.m
+++ sm/insts/labbrick/labBrickLoadLibrary.m
@@ -2,11 +2,15 @@ function [ret,warnings]=labBrickLoadLibrary
 % Load the lab brick driver library.  Return true on success.
 ret = true;
 if ~libisloaded('vnx_fsynth')  
-    p=strrep(which('smcLabBrick'),'smcLabBrick.m','labbrick') ;    
-    libname = '\SDK\vnx_fsynth.dll'; 
-    headname = '\SDK\vnx_LSG_api.h'; 
-    [notFound,warnings] = loadlibrary([p libname],[p headname]); % notFound is list of functions not found, useful for debugging. 
+    libraryPath=strrep(which('smcLabBrick'),'smcLabBrick.m','labbrick') ;    
+    dirEnd = strfind(libraryPath,'\'); 
+    libraryPath = libraryPath(1:dirEnd(end-1)); 
+    libname = 'labbrick\SDK\vnx_fsynth.dll'; 
+    headname = 'labbrick\SDK\vnx_LSG_api.h'; 
+    [notFound,warnings] = loadlibrary([libraryPath libname],[libraryPath headname]); % notFound is list of functions not found, useful for debugging. 
     if ~libisloaded('vnx_fsynth')
         ret=false;
     end
+else
+    warnings = ''; 
 end
